c274ccb837df7d862d004468d4f098bbf9a057fd
John Borden <johnborden23@gmail.com>
2026-07-31 20:22:47 -0500

    Moved to an older version of Pharo

--- scripts/visualizeCoverage.sh
+++ scripts/visualizeCoverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # curl -L https://get.pharo.org/64 | bash
-wget -O- https://get.pharo.org | bash
+wget -O- https://get.pharo.org/10 | bash
 ./pharo --headless Pharo.image ./scripts/visualizeCoverage.st
