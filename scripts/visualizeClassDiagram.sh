0651d3f3fe5ca112a440b6cade18efcdeafc2193
John Borden <johnborden23@gmail.com>
2026-07-31 20:22:15 -0500

    Moved to an older Pharo version

--- scripts/visualizeClassDiagram.sh
+++ scripts/visualizeClassDiagram.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # curl -L https://get.pharo.org/64/ | bash
-wget -O- https://get.pharo.org | bash
+wget -O- https://get.pharo.org/10 | bash
 ./pharo --headless Pharo.image ./scripts/visualizeClassDiagram.st
